5c8915fe136fe6bcd1d8ac4d508df9166f6fb0f5
khuraibet <129894070+khuraibet@users.noreply.github.com>
2025-04-08 06:03:11 +0000

    Added domain model outline

--- FinalProject/Final/ConsoleUI.cs
+++ FinalProject/Final/ConsoleUI.cs
@@ -24,14 +24,30 @@ public class ConsoleUI {
                 })
         );
 
+        string addAnotherIngredient;
+
         if(mode == "Add a Recipe") {
             do {
-                
                 string recipeName = AskForInput("Enter recipe name: ");
-
                 string sourceName = AskForInput("Enter source of recipe (name or online): ");
+                List<Ingredient> ingredients = new List<Ingredient>();
 
-                fileSaver.AppendLine(recipeName + ":" + sourceName);
+                Recipe recipe = new Recipe(recipeName, sourceName, ingredients);
+
+                do {
+                    recipe.Ingredients.Add(new Ingredient(AskForInput("Enter an ingredient: ")));
+                    addAnotherIngredient = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                            .Title("What would you like to do?")
+                            .AddChoices(new[] {
+                                "Add another",
+                                "That's all"
+                            })
+                    );
+                } while(addAnotherIngredient != "That's all");
+                
+                string formattedIngredients = string.Join(", ", recipe.Ingredients.Select(i => i.Name));
+                fileSaver.AppendLine($"Recipe: {recipe.Name}, Source: {recipe.SourceName}, Ingredients: {formattedIngredients}");
 
                 command = AnsiConsole.Prompt(
                     new SelectionPrompt<string>()
@@ -47,6 +63,8 @@ public class ConsoleUI {
 
             } while(command != "End");
         }
+
+        // Need to add functionality for other commands
 
         Console.Write("Program ended");
     }
---
+++ FinalProject/Final/Domain.cs
@@ -0,0 +1,33 @@
+namespace Final;
+
+public class Ingredient {
+    public string Name { get; }
+
+    public Ingredient(string name) {
+        this.Name = name;
+    }
+
+    public override string ToString() {
+        return this.Name;
+    }
+}
+
+public class Recipe {
+    public string Name { get; }
+    public string SourceName { get; }
+    public List<Ingredient> Ingredients { get; }
+
+    public Recipe(string name, string sourceName, List<Ingredient> ingredients) {
+        this.Name = name;
+        this.SourceName = sourceName;
+        this.Ingredients = ingredients;
+    }
+}
+
+public class GroceryList {
+    public string ItemName { get; }
+
+    public GroceryList(string name) {
+        this.ItemName = name;
+    }
+}
