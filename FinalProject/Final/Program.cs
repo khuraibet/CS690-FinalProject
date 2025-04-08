6207f06ebbc429ca8b72bfc6a64e0529efdf238f
khuraibet <129894070+khuraibet@users.noreply.github.com>
2025-04-08 03:22:27 +0000

    initial code sequence

--- FinalProject/Final/Program.cs
+++ FinalProject/Final/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string command; 
+
+        do {
+            
+            Console.Write("Enter recipe name: ");
+            string recipeName = Console.ReadLine();
+
+            Console.Write("Enter source of recipe (name or online): ");
+            string sourceName = Console.ReadLine();
+
+            File.AppendAllText("recipes.txt", recipeName + ":" + sourceName + Environment.NewLine);
+
+            Console.Write("Enter command (end or continue): ");
+            command = Console.ReadLine();
+        } while(command != "end");
+
+        Console.Write("Program ended");
     }
 }
